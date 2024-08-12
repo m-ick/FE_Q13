ea395aef6fd07e7a63ea86d15f8a463da5ae4cfa
m-ick <11mickm@gmail.com>
2024-08-12 18:00:55 +0000

    finished

--- main.cpp
+++ main.cpp
@@ -14,15 +14,15 @@ Location: 0x4353 Error Value: 0xFFFFFFFF
 #include "mbed.h"
 
 DigitalOut myled(LED1);
-UnbufferedSerial pc(USBTX, USBRX);
-UnbufferedSerial dev(PA_2, PA_3);
+UnbufferedSerial pc(USBTX, USBRX);  // Serial is deprecated in OS6. Use UnbufferedSerial
+UnbufferedSerial dev(PA_2, PA_3);   // pins 9 and 10 are called PA_2 and PA_3 (pinmap not found for peripheral)
 
 char buff;
 
 void dev_recv(){
     while(dev.readable()){
-        dev.read(&buff, 1);
-        pc.write(&buff, 1);    
+        dev.read(&buff, 1);     // pc.putc(dev.getc()) is for Serial
+        pc.write(&buff, 1);     // .read .write are for BufferedSerial and UnbufferedSerial
     }    
 }
 
@@ -35,14 +35,14 @@ void pc_recv(){
 
 int main() {
     pc.baud(9600);
-    dev.baud(115200);
+    dev.baud(115200);          // dev not device1
     pc.attach(&pc_recv);
     dev.attach(&dev_recv);
     printf("Hello!! \r\n");
     
     while(1) {
         myled = 1;
-        ThisThread::sleep_for(1s);
+        ThisThread::sleep_for(1s); // wait(1) is deprecated. could use wait_us(1000000) but is also deprecated
         myled = 0;
         ThisThread::sleep_for(1s);
     }
